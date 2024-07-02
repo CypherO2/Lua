9b5ebc6c6158ca15e70227a0df3efba150396255
Cassiopeia <charlespresley2006@outlook.com>
2024-07-02 19:33:51 +0100

    temp abandoned until more experience in Lua

--- main.lua
+++ main.lua
@@ -7,7 +7,13 @@
 -- automated recipe bot [--]
 -- twitter bot - inspiring quote [?]
 -- UI + DB [?]
+-- Weather Checker by Location [1]
 
--- PROJECT NAME :
--- LANGUAGE :
--- VERSION CONTROLLER :  
+-- PROJECT NAME : 
+-- LANGUAGE : Lua
+-- VERSION CONTROLLER : GitHub
+
+local api_key = ""
+http = require(socket.http)
+
+api_url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid="...api_key
